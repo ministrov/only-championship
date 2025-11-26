4c56153735a5b55cb21f9fe2bd7da90b3d4fa56c
Zhila <ministrov2018@gmail.com>
2025-11-26 11:47:41 +0300

    feat: create a markup for footer

--- layout/footer/Footer.tsx
+++ layout/footer/Footer.tsx
@@ -1,7 +1,57 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={styles.footer}>
+      <header className={styles.top}>
+        <div className={styles.left}>
+          <button>Начать проект</button>
+          <ul className={styles.footerSocials}>
+            <li>
+              <Link href={'#'}>be</Link>
+            </li>
+            <li>
+              <Link href={'#'}>dp</Link>
+            </li>
+            <li>
+              <Link href={'#'}>TG</Link>
+            </li>
+            <li>
+              <Link href={'#'}>VK</Link>
+            </li>
+          </ul>
+
+          <p>© 2014 - 2024</p>
+        </div>
+        <div className={styles.right}>
+          <p>Создаем качественный digital-продукт на базе стратегии, креатива и технологий</p>
+
+          <div className={styles.links}>
+            <a href="mailto:hello@only.digital">hello@only.digital</a>
+            <br />
+            <a href="tel:+74957409979">+7 (495) 740 99 79</a>
+          </div>
+
+          <div className={styles.telegram}>
+            <p>Telegram для связи</p>
+            <a href="#">@onlydigitalagency</a>
+          </div>
+
+          <div className={styles.presentation}>
+            <a href="#">pdf</a>
+            <a href="#">pitch</a>
+            <p>Презентация компании</p>
+          </div>
+
+          <Image src={'/footer-logo.svg'} width={188} height={30} alt='A footer logo' />
+        </div>
+      </header>
+      <footer className={styles.bottom}>
+        <p>Creative digital production</p>
+        <Link href={'#'}>Политика конфиденциальности</Link>
+      </footer>
+    </footer>
   )
 }
