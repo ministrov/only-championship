7d228ef952c2fa7952eb83c7d818ab0cac6fbf7f
Zhila <ministrov2018@gmail.com>
2025-11-26 11:20:49 +0300

    feat: create a markup for a header

--- layout/header/Header.tsx
+++ layout/header/Header.tsx
@@ -6,7 +6,24 @@ export const Header = () => {
     <header className={styles.header}>
       <div className="container">
         <Image src={'/logo.svg'} width={82} height={27} alt={'Design studio logo'} />
-        Header
+
+        <nav className={styles.navigation}>
+          <ul className={styles.mainNav}>
+            <li>Проекты</li>
+            <li>Компания</li>
+            <li>Направления</li>
+            <li>Контакты</li>
+            <li>Блог</li>
+            <li>Карьера</li>
+          </ul>
+
+          <div className={styles.favorite}>
+            <span>40</span>
+            <button type='button'>
+              <Image src={'/star.svg'} width={10} height={10} alt={'A favorite button as a star icon'} />
+            </button>
+          </div>
+        </nav>
       </div>
     </header>
   )
