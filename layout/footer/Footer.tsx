cc85e2b2d0c9fa6660efcd8450f14a82c8415a64
Zhila <ministrov2018@gmail.com>
2025-11-26 16:45:45 +0300

    feat: end up creating styling for desktop footer

--- layout/footer/Footer.tsx
+++ layout/footer/Footer.tsx
@@ -26,26 +26,30 @@ export const Footer = () => {
           <p>© 2014 - 2024</p>
         </div>
         <div className={styles.right}>
-          <p>Создаем качественный digital-продукт на базе стратегии, креатива и технологий</p>
+          <div className={styles.rightText}>
+            <p>Создаем качественный digital-продукт на базе стратегии, креатива и технологий</p>
 
-          <div className={styles.links}>
-            <a href="mailto:hello@only.digital">hello@only.digital</a>
-            <br />
-            <a href="tel:+74957409979">+7 (495) 740 99 79</a>
+            <div className={styles.links}>
+              <a href="mailto:hello@only.digital">hello@only.digital</a>
+              <br />
+              <a href="tel:+74957409979">+7 (495) 740 99 79</a>
+            </div>
+
+            <div className={styles.telegram}>
+              <p>Telegram для связи</p>
+              <a href="#">@onlydigitalagency</a>
+            </div>
+
+            <div className={styles.presentation}>
+              <a href="#">pdf</a>
+              <a href="#">pitch</a>
+              <p>Презентация компании</p>
+            </div>
           </div>
 
-          <div className={styles.telegram}>
-            <p>Telegram для связи</p>
-            <a href="#">@onlydigitalagency</a>
+          <div className={styles.rightImage}>
+            <Image src={'/footer-logo.svg'} width={188} height={30} alt='A footer logo' />
           </div>
-
-          <div className={styles.presentation}>
-            <a href="#">pdf</a>
-            <a href="#">pitch</a>
-            <p>Презентация компании</p>
-          </div>
-
-          <Image src={'/footer-logo.svg'} width={188} height={30} alt='A footer logo' />
         </div>
       </header>
       <footer className={styles.bottom}>
