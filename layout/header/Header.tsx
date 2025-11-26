2a4e66222bbc2e92cb1a790e54806ae26a9e4c53
Zhila <ministrov2018@gmail.com>
2025-11-26 13:25:41 +0300

    feat: end up creating a header styles for desktop viewport 1920px

--- layout/header/Header.tsx
+++ layout/header/Header.tsx
@@ -6,40 +6,42 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className="container">
-        <Link href={'/'} className={styles.logo}>
-          <Image src={'/logo.svg'} width={82} height={27} alt={'Design studio logo'} />
-          <span className="visually-hidden">На главную</span>
-        </Link>
+        <div className={styles.wrapper}>
+          <Link href={'/'} className={styles.logo}>
+            <Image src={'/logo.svg'} width={82} height={27} alt={'Design studio logo'} />
+            <span className="visually-hidden">На главную</span>
+          </Link>
 
-        <nav className={styles.navigation}>
-          <ul className={styles.mainNav}>
-            <li>
-              <Link href={'#'}>Проекты</Link>
-            </li>
-            <li>
-              <Link href={'#'}>Компания</Link>
-            </li>
-            <li>
-              <Link href={'#'}>Направления</Link>
-            </li>
-            <li>
-              <Link href={'#'}>Контакты</Link>
-            </li>
-            <li>
-              <Link href={'#'}>Блог</Link>
-            </li>
-            <li>
-              <Link href={'#'}>Карьера</Link>
-            </li>
-          </ul>
+          <nav className={styles.navigation}>
+            <ul className={styles.mainNav}>
+              <li>
+                <Link href={'#'}>Проекты</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Компания</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Направления</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Контакты</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Блог</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Карьера</Link>
+              </li>
+            </ul>
 
-          <div className={styles.favorite}>
-            <span>40</span>
-            <button type='button'>
-              <Image src={'/star.svg'} width={10} height={10} alt={'A favorite button as a star icon'} />
-            </button>
-          </div>
-        </nav>
+            <div className={styles.favorite}>
+              <span>40</span>
+              <button type='button' className={styles.favoriteBtn}>
+                <Image src={'/star.svg'} width={24} height={24} alt={'A favorite button as a star icon'} />
+              </button>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   )
