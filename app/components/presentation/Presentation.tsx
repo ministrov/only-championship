31cdcab6ca21f56666c4caadca6addb2acb76274
Zhila <ministrov2018@gmail.com>
2025-11-27 17:12:22 +0300

    feat: decomposite presentation from footer to presentation component

---
+++ app/components/presentation/Presentation.tsx
@@ -0,0 +1,11 @@
+import styles from './Presentation.module.css';
+
+export const Presentation = () => {
+  return (
+    <div className={styles.presentation}>
+      <a href="#">pdf</a>
+      <a href="#">pitch</a>
+      <p>Презентация компании</p>
+    </div>
+  )
+}
