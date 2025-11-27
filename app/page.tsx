fdce0b53676480d1bc9325aab8bff999cf77697b
Zhila <ministrov2018@gmail.com>
2025-11-27 18:28:32 +0300

    feat: create a basic structure for section hero and portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Hero } from './components/hero/Hero';
+import { Portfolio } from './components/portfolio/Portfolio';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className='container'>
+      <Hero />
+      <Portfolio />
       <p>Main</p>
 
       <h1 className={styles.h1}>Creative digital production</h1>
