9bd0fea66997d0c7e73b51f20fd1f6b2bd9eee66
Zhila <ministrov2018@gmail.com>
2025-11-28 14:54:45 +0300

    feat: start creating a markup for projects section and export icon for carousel controls

--- sections/projects/Projects.tsx
+++ sections/projects/Projects.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/image';
 import styles from './Projects.module.css';
 
 export const Projects = () => {
   return (
     <section className={styles.projects}>
       <h2 className={styles.title}>крупные проекты</h2>
+      <p>Поддерживаем и развиваем долгосрочное сотрудничество</p>
+
+      <button className={styles.left}>
+        <Image src={'/arrow-left.svg'} width={20} height={20} alt={''} />
+      </button>
+      <button className={styles.right}>
+        <Image src={'/arrow-right.svg'} width={20} height={20} alt={''} />
+      </button>
+
+      <div className={styles.carousel}>
+        <div className={styles.item}>
+
+        </div>
+        <div className={styles.item}></div>
+        <div className={styles.item}></div>
+        <div className={styles.item}></div>
+      </div>
     </section>
   )
 }
