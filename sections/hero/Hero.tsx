362f9ba4a4215604a060e54866f8ee1ccaa47632
Zhila <ministrov2018@gmail.com>
2025-11-28 11:36:44 +0300

    feat: start creating a markup for a hero section and add a social icon and video placeholder for figma

--- sections/hero/Hero.tsx
+++ sections/hero/Hero.tsx
@@ -1,9 +1,42 @@
+import Image from 'next/image';
 import styles from './Hero.module.css';
 
 export const Hero = () => {
   return (
     <section className={styles.hero}>
       <h2 className='visually-hidden'>Section hero</h2>
+
+      <p className={styles.slogan}>Creative digital production</p>
+
+      <Image src={'/hero-video-img.jpeg'} width={608} height={304} alt={'A placeholder for future video'} />
+      <ul className={styles.social}>
+        <li>
+          <span>25</span>
+          <Image src={'/social-icon-1.svg'} width={28} height={28} alt={''} />
+        </li>
+        <li>
+          <span>1</span>
+          <Image src={''} width={40} height={12} alt={''} />
+        </li>
+        <li>
+          <span>26</span>
+          <Image src={''} width={25} height={25} alt={''} />
+        </li>
+        <li>
+          <span>22</span>
+          <Image src={''} width={23} height={29} alt={''} />
+        </li>
+        <li>
+          <span>40</span>
+          <Image src={''} width={33} height={34} alt={''} />
+        </li>
+      </ul>
+
+      <p>
+        <span>10</span> <span>лет</span> <span>Лучший креативный разработчик для b2b</span>
+      </p>
+
+
     </section>
   )
 }
