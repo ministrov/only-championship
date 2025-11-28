7ba1637746828354207e4923efcee9985cde4505
Zhila <ministrov2018@gmail.com>
2025-11-28 14:27:22 +0300

    feat: start creating markup for clients section and download a figma png

--- sections/clients/Clients.tsx
+++ sections/clients/Clients.tsx
@@ -1,9 +1,51 @@
+import Image from 'next/image';
 import styles from './Clients.module.css';
 
 export const Clients = () => {
   return (
     <section className={styles.clients}>
       <h2 className='visually-hidden'>Section of the Only clients</h2>
+
+      <p>20 клиентов — крупнейшие компании из списка Forbes. Более 100 реализованных проектов</p>
+
+      <ul className={styles.clients}>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+        <li className={styles.client}>
+          <Image src={''} width={12} height={12} alt={''} />
+        </li>
+      </ul>
     </section>
   )
 }
