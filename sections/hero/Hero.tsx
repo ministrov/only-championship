75da3520775a1466f79cda73eac44f650d64a65b
Zhila <ministrov2018@gmail.com>
2025-12-02 15:04:48 +0300

    feat: move social list from hero section to heroSocial component to decomposite markup properly

--- sections/hero/Hero.tsx
+++ sections/hero/Hero.tsx
@@ -4,6 +4,7 @@ import { TgContact } from '@/app/components/tgContact/TgContact';
 import { Presentation } from '@/app/components/presentation/Presentation';
 import { Button } from '@/app/components/button/Button';
 import styles from './Hero.module.css';
+import { HeroSocials } from '@/app/components/heroSosials/HeroSocials';
 
 export const Hero = () => {
   return (
@@ -16,28 +17,8 @@ export const Hero = () => {
         <div className={styles.image}>
           <Image src={'/hero-video-img.png'} width={608} height={304} alt={'A placeholder for future video'} />
         </div>
-        <ul className={styles.social}>
-          <li>
-            <span>25</span>
-            <Image src={'/social-icon-1.svg'} width={28} height={28} alt={''} />
-          </li>
-          <li>
-            <span>1</span>
-            <Image src={'/social-icon-2.svg'} width={40} height={12} alt={''} />
-          </li>
-          <li>
-            <span>26</span>
-            <Image src={'/social-icon-3.svg'} width={25} height={25} alt={''} />
-          </li>
-          <li>
-            <span>22</span>
-            <Image src={'/social-icon-4.svg'} width={23} height={29} alt={''} />
-          </li>
-          <li>
-            <span>40</span>
-            <Image src={'/social-icon-5.svg'} width={33} height={34} alt={''} />
-          </li>
-        </ul>
+
+        <HeroSocials />
       </header>
 
       <footer className={styles.bottom}>
