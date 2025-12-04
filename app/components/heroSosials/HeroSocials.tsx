ff49e7d3fcd27cf073fce51891441705760cefa3
Zhila <ministrov2018@gmail.com>
2025-12-04 14:11:33 +0300

    keep on operating with pixel perfect and fit project to figma maket with hero section on 1920

--- app/components/heroSosials/HeroSocials.tsx
+++ app/components/heroSosials/HeroSocials.tsx
@@ -5,34 +5,44 @@ export const HeroSocials = () => {
   return (
     <ul className={styles.social}>
       <li className={styles.socialItem}>
-        <span className={styles.number}>25</span>
-        <div className={styles.image}>
-          <Image src={'/social-icon-1.svg'} width={28} height={28} alt={''} />
-        </div>
+        <a className={styles.link} href="#">
+          <span className={styles.number}>25</span>
+          <div className={styles.image}>
+            <Image src={'/social-icon-1.svg'} width={28} height={28} alt={''} />
+          </div>
+        </a>
       </li>
       <li className={styles.socialItem}>
-        <span className={styles.number}>1</span>
-        <div className={styles.image}>
-          <Image src={'/social-icon-2.svg'} width={40} height={12} alt={''} />
-        </div>
+        <a className={styles.link} href="#">
+          <span className={styles.number}>1</span>
+          <div className={styles.image}>
+            <Image src={'/social-icon-2.svg'} width={40} height={12} alt={''} />
+          </div>
+        </a>
       </li>
       <li className={styles.socialItem}>
-        <span className={styles.number}>26</span>
-        <div className={styles.image}>
-          <Image src={'/social-icon-3.svg'} width={25} height={25} alt={''} />
-        </div>
+        <a className={styles.link} href="#">
+          <span className={styles.number}>26</span>
+          <div className={styles.image}>
+            <Image src={'/social-icon-3.svg'} width={25} height={25} alt={''} />
+          </div>
+        </a>
       </li>
       <li className={styles.socialItem}>
-        <span className={styles.number}>22</span>
-        <div className={styles.image}>
-          <Image src={'/social-icon-4.svg'} width={23} height={29} alt={''} />
-        </div>
+        <a className={styles.link} href="#">
+          <span className={styles.number}>22</span>
+          <div className={styles.image}>
+            <Image src={'/social-icon-4.svg'} width={23} height={29} alt={''} />
+          </div>
+        </a>
       </li>
       <li className={styles.socialItem}>
-        <span className={styles.number}>40</span>
-        <div className={styles.image}>
-          <Image src={'/social-icon-5.svg'} width={33} height={34} alt={''} />
-        </div>
+        <a className={styles.link} href="#">
+          <span className={styles.number}>40</span>
+          <div className={styles.image}>
+            <Image src={'/social-icon-5.svg'} width={33} height={34} alt={''} />
+          </div>
+        </a>
       </li>
     </ul>
   )
